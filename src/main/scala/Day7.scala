5697123212634f3f9c6b8b858e6eb3ceeb594282
Joe Horne <joeh19@gmail.com>
2022-12-07 09:50:40 -0500

    Added more spacing

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -1,11 +1,14 @@
 case class Path(elements: String*):
   def +(element: String) = Path(elements :+ element*)
   def parent: Path       = Path(elements.dropRight(1)*)
+
 case class Directory(path: Path, files: Seq[File], dirs: Seq[Path]):
   def addFile(file: File): Directory = this.copy(files = files :+ file)
   def addDir(dir: Path): Directory   = this.copy(dirs = dirs :+ dir)
+
   def size(directories: Map[Path, Directory]): Int =
     files.map(_.size).sum + dirs.map(directories(_).size(directories)).sum
+
 case class File(name: String, size: Int)
 
 @main
@@ -15,21 +18,28 @@ def day7 =
   val dir   = raw"dir (\w+)".r
   val file  = raw"(\d+) ([\w\.]+)".r
   val root  = Path("/")
+
   val (_, directories) = input.foldLeft((root, Map(root → Directory(root, Seq.empty, Seq.empty)))) {
+
     case ((_, directories), cd("/"))    ⇒ (root, directories)
     case ((pwd, directories), cd("..")) ⇒ (pwd.parent, directories)
+
     case ((pwd, directories), cd(dir)) ⇒
       val path      = pwd + dir
       val directory = directories.getOrElse(path, Directory(path, Seq.empty, Seq.empty))
       (path, directories + (path → directory))
+
     case ((pwd, directories), file(size, name)) ⇒
       val directory = directories(pwd).addFile(File(name, size.toInt))
       (pwd, directories + (pwd → directory))
+
     case ((pwd, directories), dir(dir)) ⇒
       val directory = directories(pwd).addDir(pwd + dir)
       (pwd, directories + (pwd → directory))
+
     case ((pwd, directories), "$ ls") ⇒ (pwd, directories)
   }
+
   val sizes = directories.view.mapValues(_.size(directories)).toMap
   val sum   = sizes.values.filter(_ <= 100000).sum
   println(sum)
