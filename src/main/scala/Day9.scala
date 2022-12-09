6a701bc55c97e94936ed3881b1059e0716b93f50
Joe Horne <joeh19@gmail.com>
2022-12-09 08:13:17 -0500

    Inlined something

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -16,17 +16,17 @@ def day9 =
 def simulate(input: Seq[String], n: Int): Int =
   val re     = raw"([UDLR]) (\d+)".r
   val dirmap = Map("U" → Pos(0, 1), "D" → Pos(0, -1), "L" → Pos(-1, 0), "R" → Pos(1, 0))
-  val allpos = Seq.fill(n)(Pos.zero)
 
-  val (_, seen) = input.foldLeft((allpos, Set(Pos.zero))) { case ((pos, seen), re(dirstr, dist)) ⇒
-    val dir = dirmap(dirstr)
-    (0 until dist.toInt).foldLeft((pos, seen)) { case ((pos, seen), _) ⇒
-      val newHead = pos.head + dir
-      val newPos = pos.tail.foldLeft(Seq(newHead)) { case (pos, tail) ⇒
-        pos :+ (tail + (pos.last - tail).move)
+  val (_, seen) = input.foldLeft((Seq.fill(n)(Pos.zero), Set(Pos.zero))) {
+    case ((pos, seen), re(dirstr, dist)) ⇒
+      val dir = dirmap(dirstr)
+      (0 until dist.toInt).foldLeft((pos, seen)) { case ((pos, seen), _) ⇒
+        val newHead = pos.head + dir
+        val newPos = pos.tail.foldLeft(Seq(newHead)) { case (pos, tail) ⇒
+          pos :+ (tail + (pos.last - tail).move)
+        }
+        (newPos, seen + newPos.last)
       }
-      (newPos, seen + newPos.last)
-    }
   }
 
   seen.size
